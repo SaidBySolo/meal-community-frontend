63446ae16d2718b037e51aa3afb3b3695c9a9a26
Ryu Juheon <saidbysolo@gmail.com>
2025-06-11 14:12:11 +0900

    Revert "Merge branch 'main' into logout-button"
    This reverts commit bcf5e9905bf7d54714f95a7585ca99ac4d5a5b1c, reversing
    changes made to ed3bbaf12c24839a4ab8c15fb3a4daee6766c521.

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,30 +1,31 @@
 // 로그아웃 버튼 기능 구현, 세션 삭제
-import { Button as RadixButton } from "@radix-ui/themes";
-import { requestLogout } from "../api";
+import { Button as RadixButton } from "@radix-ui/themes"
+import { requestLogout } from "../api"
 
-function LogoutButton() {
-  const handleLogout = async () => {
-    console.log("로그아웃 버튼 눌림");
+function LogoutButton(){
+    const handleLogout = async () => {
+        
+        console.log("로그아웃 버튼 눌림");
 
-    localStorage.removeItem("access_token");
+        localStorage.removeItem("access_token");
 
-    try {
-      await requestLogout();
-    } catch (err) {
-      console.error("로그아웃 실패:", err);
-      return; // 오류 발생 시, 이동하지 않도록 막기
-    }
+        try {
+            await requestLogout();
+        } catch (err) {
+            console.error("로그아웃 실패:", err);
+            return;  // 오류 발생 시, 이동하지 않도록 막기
+        }
+        
+        window.location.href = "/";
+};
 
-    window.location.href = "/";
-  };
-
-  return (
-    <p>
-      <RadixButton radius="full" variant="soft" onClick={handleLogout}>
-        로그아웃
-      </RadixButton>
-    </p>
-  );
+    return (
+        <p>
+            <RadixButton radius="full" variant="soft" onClick={handleLogout}>
+                로그아웃 버튼
+            </RadixButton>
+        </p>
+    )
 }
 
-export default LogoutButton;
+export default LogoutButton
